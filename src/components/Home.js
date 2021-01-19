2d9ba01487d38d981c52ba357bdb3d2babc57cd0
YashRTiwari <yrtiean@gmail.com>
2021-01-18 19:23:22 -0600

    Fetching Data from json server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,33 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import BlogList from './BlogList.js';
 import './Home.css';
 
 export default function Home() {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+    const [blogs, setBlogs] = useState(null)
 
     const handleDelete = (id) => {
         const newBlogs = blogs.filter( blog => blog.id !== id )
         setBlogs(newBlogs)
     }
 
+    // Called -> Everytime component is rendered
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(response => {
+            return response.json()
+        }) // returns a promise
+        .then( (data) => {
+            setBlogs(data)
+        })
+    }, []) // [] -> avoids re-render, i.e. will only runs once
+
     return (
         <div>
-            <BlogList blogs ={blogs} title="Hello World" handleDelete={handleDelete}/>   
+            {
+                blogs && <BlogList blogs ={blogs} title="Hello World" handleDelete={handleDelete}/> // && conditional template
+            }
+               
         </div>
     )
 }
